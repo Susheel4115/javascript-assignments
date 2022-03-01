bc156d43ee11763a2b37a4ad038c9612bf8cb6df
Mr.Susheel Kumar <suseelkumar4115@gmail.com>
2022-03-01 21:08:25 +0530

    This is my first commit

--- assignments/favourite_movie/favourite_movie.js
+++ assignments/favourite_movie/favourite_movie.js
@@ -1,7 +1,14 @@
 const movies = [];
 
 function favouriteMovie(operation, movie) {
-    // Write your code here
+  // Write your code here
+  if (operation === "add") {
+    movies.push(movie);
+  }
+  if (operation === "remove") {
+    movies.pop();
+  }
+  return movies;
 }
 
 module.exports = favouriteMovie;
