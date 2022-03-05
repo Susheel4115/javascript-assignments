016c7e9ea708fb8a155144d4f3cf840647b7e3d5
Mr.Susheel Kumar <suseelkumar4115@gmail.com>
2022-03-05 12:11:58 +0530

    This is my first commit on array to object change.

--- assignments/create_object/create_object.js
+++ assignments/create_object/create_object.js
@@ -1,5 +1,22 @@
 function CreateObject(arr) {
-    // Write your code here
+  // Write your code here
+  let keys = [],
+    values = [],
+    obj = {};
+  for (let i = 0; i < arr.length; i++) {
+    if (i === 0 || i % 2 === 0) {
+      keys.push(arr[i]);
+    } else {
+      values.push(arr[i]);
+    }
+  }
+  for (let i = 0; i < keys.length; i++) {
+    obj[keys[i]] = values[i];
+  }
+  return obj;
+  //   let obj = Object.fromEntries(arr);
 }
 
 module.exports = CreateObject;
+
+// const arr = ["firstName", "Yash", "lastName", "Goyal"];
