c1250b93607131b4a0bdcf52e71109dbcda1f1d5
Mr.Susheel Kumar <suseelkumar4115@gmail.com>
2022-03-14 10:21:22 +0530

    first coommit on my fetch api

--- assignments/todo_list/script.js
+++ assignments/todo_list/script.js
@@ -1,12 +1,68 @@
 const newTodoInput = document.querySelector("input");
 const addTodoInputbutton = document.querySelector("button");
 const newTodoInputList = document.querySelector("div.todos");
-let count = 0;
+const completedTodosButton = document.querySelector(".btn1");
+const uncompletedTodosButton = document.querySelector(".btn2");
+
 addTodoInputbutton.addEventListener("click", () => {
-  let content = document.createElement("p");
-  content.setAttribute("key", count);
-  content.innerHTML = newTodoInput.value;
-  newTodoInputList.append(content);
-  count = count + 1;
-  content.addEventListener("click", content.remove);
+  fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      let li = `<tr><th>User id</th><th>Descrption</th><th>Status</th></tr>,`;
+
+      json.forEach((user) => {
+        li += `<tr>
+                <td>${user.id} </td>
+                <td>${user.title}</td>  
+                <td>${user.completed ? "completed" : "pending"}</td>    
+            </tr>`;
+        document.getElementById("users").innerHTML = li;
+      });
+    })
+    .catch((err) => console.log(err));
+});
+
+completedTodosButton.addEventListener("click", () => {
+  fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      let li = `<tr><th>User id</th><th>Descrption</th><th>Status</th></tr>,`;
+
+      json.forEach((user) => {
+        if (user.completed) {
+          li += `<tr>
+          <td>${user.id} </td>
+          <td>${user.title}</td>  
+          <td>completed-${user.completed}</td>    
+          </tr>`;
+          document.getElementById("users").innerHTML = li;
+        }
+      });
+    })
+    .catch((err) => console.log(err));
+});
+uncompletedTodosButton.addEventListener("click", () => {
+  fetch("https://jsonplaceholder.typicode.com/todos", {
+    method: "GET",
+  })
+    .then((response) => response.json())
+    .then((json) => {
+      let li = `<tr><th>User id</th><th>Descrption</th><th>Status</th></tr>,`;
+
+      json.forEach((user) => {
+        if (!user.completed) {
+          li += `<tr>
+                <td>${user.id} </td>
+                <td>${user.title}</td>  
+                <td>Completed-${user.completed}</td>    
+            </tr>`;
+          document.getElementById("users").innerHTML = li;
+        }
+      });
+    })
+    .catch((err) => console.log(err));
 });
